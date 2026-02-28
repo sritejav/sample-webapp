f3e41fdc4528226f4dab7fd9f15c533e63178c4a
Teja <sritejaklu@gmail.com>
2026-03-01 02:17:44 +0530

    including user-date.sh in main.tf

--- sw-terraform/main.tf
+++ sw-terraform/main.tf
@@ -4,8 +4,11 @@ resource "aws_instance" "MyInstance" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+  user_data = file("user-date.sh")
+
   tags = {
-    Name        = "Web_server"
-    Environment = var.environment
+    Name = "Web_server"
+    Environment  = var.environment
+    Project_name = var.project_name
   }
 }
